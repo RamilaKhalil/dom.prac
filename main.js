57beed85f0af5dfd71972fd5a85814a2ac2c0476
Ramila Khalil <ramilamirker00@gmail.com>
2024-09-24 16:38:58 +0500

    all code

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
- var bulb = document.querySelector ("#bulb");
- var btn = document.querySelector ("button");
+  let bulb = document.querySelector ("#bulb");
+  let btn = document.querySelector ("button");
 
+let flag = 0;
 // @ts-ignore
 btn.addEventListener("click", function () { 
+     if (flag == 0) {
         // @ts-ignore
         bulb.style.backgroundColor = "yellow";
+        console.log("on");
+        flag = 1;
+        
+     } else {
+        // @ts-ignore
+        bulb.style.backgroundColor = "gray";
+        console.log("off");
+        flag = 0;
+     }
+        
 });
